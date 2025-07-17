9774c2bce41a02f7a74db8c16b1433fe411475db
robertjchandler <robertjchandler4@gmail.com>
2025-07-17 16:46:08 -0500

    add repl

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := scanner.Text()
+		cleanText := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", cleanText[0])
+	}
 }
